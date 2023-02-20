2b7be9a6a46c9ffddfeebc22602d140d88f62e58
AhmedKamruddin <98866395+AhmedKamruddin@users.noreply.github.com>
2023-02-20 09:21:07 +0530

    Add files via upload

--- Assignment 3 Circle & Ellipse/midpoint_ellipse.cpp
+++ Assignment 3 Circle & Ellipse/midpoint_ellipse.cpp
@@ -15,7 +15,73 @@ void display()
     glPointSize(2.0);
 	glBegin(GL_POINTS);
 
-    //To complete
+    int rx=10, ry=15;
+	int xc=50, yc=50;
+	float dx, dy, p1, p2, x, y;
+	x=0;
+	y=ry;
+
+	p1=ry*ry + 0.25*rx*rx - rx*rx*ry;
+	dx=2*ry*ry*x;
+	dy=2*rx*rx*y;
+
+	while(dx<dy)
+	{
+		glVertex2d(x+xc, y+yc);
+		glVertex2d(-x+xc, y+yc);
+		glVertex2d(x+xc, -y+yc);
+		glVertex2d(-x+xc, -y+yc);
+
+		// cout<<x+xc<<", "<<y+yc<<endl;
+		// cout<<-x+xc<<", "<<y+yc<<endl;
+		// cout<<x+xc<<", "<<-y+yc<<endl;
+		// cout<<-x+xc<<", "<<-y+yc<<endl;
+
+		if(p1<0)
+		{
+			x++;
+			dx=dx + (2*ry*ry);
+			p1=p1 + dx + ry*ry;
+		}
+		else
+		{
+			x++;
+			y--;
+			dx=dx + (2*ry*ry);
+			dy=dy - (2*rx*rx);
+			p1=p1 + dx - dy + ry*ry;
+		}
+	}
+
+	p2=(ry*ry*(x+0.5)*(x+0.5)) + rx*rx*(y-1)*(y-1) - rx*rx*ry*ry;
+	while(y>=0)
+	{
+		glVertex2d(x+xc, y+yc);
+		glVertex2d(-x+xc, y+yc);
+		glVertex2d(x+xc, -y+yc);
+		glVertex2d(-x+xc, -y+yc);
+
+		// cout<<x+xc<<", "<<y+yc<<endl;
+		// cout<<-x+xc<<", "<<y+yc<<endl;
+		// cout<<x+xc<<", "<<-y+yc<<endl;
+		// cout<<-x+xc<<", "<<-y+yc<<endl;
+
+		if(p2>0)
+		{
+			y--;
+			dy=dy-(2*rx*rx);
+			p2=p2+(rx*rx)-dy;
+		}
+		else
+		{
+			y--;
+			x++;
+			dx=dx+(2*ry*ry);
+			dy=dy-(2*rx*rx);
+			p2=p2+dx-dy+rx*rx;
+		}
+	}
+
 
     glEnd();
 	glFlush();
@@ -30,5 +96,6 @@ int main(int argc, char **argv)
 	glutCreateWindow("Ellipse Midpoint");
 	myInit();
 	glutDisplayFunc(display);
-	glutMainLoop();   
+	glutMainLoop();
+	// display();   
 }
